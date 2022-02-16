92e32f89201ca304dccdb582b41523f75c5f273a
PiyushBarde <piyushbarde1@gmail.com>
2022-02-16 22:51:34 +0530

    UPDATED: pass name as attributes

--- src/main/java/com/bridgelabz/restapicalls/controller/RestAPICallsController.java
+++ src/main/java/com/bridgelabz/restapicalls/controller/RestAPICallsController.java
@@ -2,9 +2,13 @@ package com.bridgelabz.restapicalls.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bridgelabz.restapicalls.model.User;
 
 @RestController
 @RequestMapping("/hello")
@@ -16,5 +20,9 @@ public class RestAPICallsController {
 	@GetMapping(("/param/{name}"))
 	public String sayHello1(@PathVariable String name){
 		return "Hello " + name + " from BridgeLabz";
+	}
+	@PostMapping("/post")
+	public String sayHello(@RequestBody User user) {
+		return "Hello " + user.getFirstName() + " " + user.getLastName() + " from bridgelabz";
 	}
 }
