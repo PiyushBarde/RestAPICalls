4baefaebdf1bed3a908820ff85e3561f302d4e98
PiyushBarde <piyushbarde1@gmail.com>
2022-02-16 22:15:59 +0530

    UPDATED: name as query parameter

--- src/main/java/com/bridgelabz/restapicalls/controller/RestAPICallsController.java
+++ src/main/java/com/bridgelabz/restapicalls/controller/RestAPICallsController.java
@@ -1,12 +1,16 @@
 package com.bridgelabz.restapicalls.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/hello")
 public class RestAPICallsController {
-	@GetMapping("/hello")
-	public String sayHello(){
-		return "Hello from BridgeLabz";
+	@GetMapping("/query")
+	public String sayHello(@RequestParam(value="name")String name){
+		return "Hello " + name + " from BridgeLabz";
 	}
 }
